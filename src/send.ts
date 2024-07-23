b147ecad5b6b98985c488806888c7dfdab2367f7
Stefan Kruik <s.kruik@icloud.com>
2024-07-23 16:58:16 +0200

    Exchanges & Stelleri Connection

--- src/send.ts
+++ src/send.ts
@@ -2,6 +2,7 @@ import amqp from "amqplib/callback_api";
 import dotenv from "dotenv";
 import { log } from "./utils/logger";
 import { getConnectionOptions } from "./utils/auth";
+import { UplinkMessage } from "./customTypes";
 dotenv.config();
 const queue: string | undefined = process.env.AMQP_MAIN_QUEUE;
 if (!queue) throw new Error("AQMP queue name missing.");
@@ -10,15 +11,22 @@ amqp.connect(getConnectionOptions(), (error0, connection) => {
     if (error0) throw error0;
 
     connection.createChannel((error1, channel) => {
+        // Setup
         if (error1) throw error1;
-        channel.assertQueue(queue, {
-            durable: false
-        });
+        channel.assertQueue(queue, { durable: false });
+        channel.assertExchange("bot-exchange", "direct", { durable: false });
 
         // Send
-        const message: string = "Hello Uplink Main!";
-        channel.sendToQueue(queue, Buffer.from(message));
-        log(`Sent message: || ${message} ||`, "info");
+        const payload: UplinkMessage = {
+            sender: "Uplink/send",
+            recipient: "Discord-Bots/Stelleri",
+            trigger_source: "Manual",
+            reason: "Testing I/O",
+            task: "log",
+            content: "Hello there Stelleri!"
+        };
+        channel.publish("bot-exchange", "Stelleri", Buffer.from(JSON.stringify(payload)));
+        log(`Sent message to || ${payload.recipient} ||`, "info");
 
         // Exit
         setTimeout(() => {
